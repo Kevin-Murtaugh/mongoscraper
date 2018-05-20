68c70d668c9d734476d1a63b1e7c7ef1a272a555
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-05-20 17:20:50 -0400

    scrape working

---
+++ controllers/scrapedDataCrud.js
@@ -0,0 +1,12 @@
+b.employee.insert( 
+    { 
+    "employee_id":1101, 
+    "name":"Prashanth", 
+    "sal":90000, 
+    "dob": new Date(1983,2,3,5,20), 
+    "department": 
+        [ 
+            'DB Amdin','DB Developer' 
+        ], 
+    "Location":"New York" 
+    });
