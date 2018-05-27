af444d6b2318de63f6e49af73113ecffa2695b85
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-05-27 11:19:12 -0400

    tweak handlebars

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const path = require("path");
 mongoose
   .connect("mongodb://localhost/kevin_scraper")
   .then(() => console.log(`Database connection successful`));
+  
 
 const Article = require("./models/article");
 
