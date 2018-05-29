a1aea4eb921d2332a5b281484c370f69b6e63a30
Kevin Murtaugh <35198999+Kevin-Murtaugh@users.noreply.github.com>
2018-05-28 21:23:57 -0400

    fix side effect

--- controllers/scrapedDataCrud.js
+++ controllers/scrapedDataCrud.js
@@ -1,36 +1,36 @@
-// var express = require("express");
-// var mongojs = require("mongojs");
+var express = require("express");
+var mongojs = require("mongojs");
 
 
-// function dbAddRecord (newDBRecord){
-//     var CheckArticle = newDBRecord.articleURL;
+function dbAddRecord (newDBRecord){
+    var CheckArticle = newDBRecord.articleURL;
 
-// // find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
-// Person.findOne({ newDBRecord.articleURL }, 'articleURL', function (err, person) {
-//   if (!err) return handleError(err);
+// find each person with a last name matching 'Ghost', selecting the `name` and `occupation` fields
+Person.findOne({ newDBRecord.articleURL }, 'articleURL', function (err, person) {
+  if (!err) return handleError(err);
 
-//   //duplicate article
+  //duplicate article
   
-//     console.log("confirmed new article");
-//     db.scraper.scrapeddata = newDBRecord;
-// });
-//    findOne()
-// }
+    console.log("confirmed new article");
+    db.scraper.scrapeddata = newDBRecord;
+});
+   findOne()
+}
 
-// // add to collection
-// db.scrapedData.insert( 
-//     { 
-//         articleTitle: String,
-//         articleURL: String,
-//         articleSummary: String,
-//         articleCommentKey: Number
-// });
+// add to collection
+db.scrapedData.insert( 
+    { 
+        articleTitle: String,
+        articleURL: String,
+        articleSummary: String,
+        articleCommentKey: Number
+});
 
-// db.scrapedData.update( 
-//     { 
-//         articleTitle: String,
-//         articleURL: String,
-//         articleSummary: String,
-//         articleCommentKey: Number
-// });
+db.scrapedData.update( 
+    { 
+        articleTitle: String,
+        articleURL: String,
+        articleSummary: String,
+        articleCommentKey: Number
+});
 
